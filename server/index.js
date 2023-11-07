2bda67d84c3acdab3ec7e49d8d3f3f674099a72e
Bismark Agyare <bismarkagyare54@gmail.com>
2023-11-07 12:20:51 +0000

    feat: Connect to database

--- server/index.js
+++ server/index.js
@@ -1,4 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to Mongodb server');
+  })
+  .catch(() => {
+    console.log('Failed to connect to Mongodb server');
+  });
 
 const app = express();
 app.listen('3005', () => {
