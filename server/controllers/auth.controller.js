923f404575f595561bdc6439072d3ffed500aa25
Bismark Agyare <bismarkagyare54@gmail.com>
2023-11-09 11:41:50 +0000

    feat: Create signin api route

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { errorhandler } from '../utils/error.js';
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -11,6 +13,37 @@ export const signup = async (req, res, next) => {
     res
       .status(201)
       .json({ message: 'User created successfully', user: newUser });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email });
+
+    if (!validUser) {
+      return next(errorhandler(404, 'User not found'));
+    }
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+    if (!validPassword) {
+      return next(errorhandler(404, 'Wrong credentials'));
+    }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    const userWithoutPassword = await User.findOne({ email }).select(
+      '-password'
+    );
+
+    res
+      .cookie('access_token', token, { httpOnly: true })
+      .status(200)
+      .json(userWithoutPassword);
   } catch (error) {
     next(error);
   }
