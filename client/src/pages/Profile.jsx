9435d9b3ccdb59ae74cca426c4c0de899dddda55
Bismark Agyare <bismarkagyare54@gmail.com>
2023-11-14 13:04:23 +0000

    feat: Add profile component

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const { currentUser } = useSelector((state) => state.user);
+  return (
+    <div>
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+      <form>
+        <img src={currentUser.avatar} alt="" />
+      </form>
+    </div>
+  );
 };
 
 export default Profile;
