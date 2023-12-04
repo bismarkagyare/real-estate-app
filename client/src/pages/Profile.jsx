972a8bd62ca23ebbb0b92fc253dd0baf52f4c135
bismarkagyare <bismarkagyare54@gmail.com>
2023-12-04 09:44:06 +0000

    feat: Complete profile page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -4,11 +4,40 @@ import { useSelector } from 'react-redux';
 const Profile = () => {
   const { currentUser } = useSelector((state) => state.user);
   return (
-    <div>
+    <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
-      <form>
-        <img src={currentUser.avatar} alt='profile' />
+      <form className='flex flex-col gap-4'>
+        <img
+          src={currentUser.avatar}
+          alt='profile'
+          className='rounded-full w-24 h-24 object-cover self-center cursor-pointer mt-2'
+        />
+        <input
+          type='text'
+          placeholder='username'
+          className='border p-3 rounded-lg'
+          id='username'
+        />
+        <input
+          type='text'
+          placeholder='email'
+          className='border p-3 rounded-lg'
+          id='email'
+        />
+        <input
+          type='text'
+          placeholder='password'
+          className='border p-3 rounded-lg'
+          id='password'
+        />
+        <button className='bg-slate-700 rounded-lg text-white p-3 uppercase hover:opacity-95 disabled:hover:opacity-80'>
+          update
+        </button>
       </form>
+      <div className='flex justify-between mt-5'>
+        <span className='text-red-700 cursor-pointer'>Delete account</span>
+        <span className='text-red-700 cursor-pointer'>Sign out</span>
+      </div>
     </div>
   );
 };
