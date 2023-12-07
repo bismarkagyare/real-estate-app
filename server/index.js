8f569663fcf1a814528ebb810eddc3ec6a12706b
bismarkagyare <bismarkagyare54@gmail.com>
2023-12-07 09:57:29 +0000

    chore: move dotenv to the top of file

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 dotenv.config();
